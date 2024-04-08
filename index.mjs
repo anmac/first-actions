c4bd2a8ee21fc189b7ff49fc7d26158ce79d0466
Miguel Maza <anmac_tec@outlook.com>
2024-04-08 18:12:56 -0500

    docs(index): add comment line

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ export const handler = async () => {
     statusCode: 200,
     body: JSON.stringify({
       message: 'firstLambdaDeploy! Your function executed successfully!',
-      number: Math.random() * 100,
+      number: Math.random() * 100, // add a comment
     }),
   };
 
